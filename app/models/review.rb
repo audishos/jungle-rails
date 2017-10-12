786f284330ee0a7f5dd58e1720156c51080768fc
audishos <audisho.sada@gmail.com>
2017-10-12 17:08:14 -0400

    Adds data validation to review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,18 @@ class Review < ApplicationRecord
 
   belongs_to :product
 
+  validates :product_id, presence: true
+  validates :user_id, presence: true
+  validates :description, presence: true, length: { minimum: 10 }
+  validates :rating, presence: true
+  validate :rating_must_be_between_1_and_10
+
+  private
+
+  def rating_must_be_between_1_and_10
+    unless self.rating >= 1 && self.rating <= 10
+      errors.add(:rating, "Must be a value between 1 and 10")
+    end
+  end
+
 end
