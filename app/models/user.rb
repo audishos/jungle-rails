4a88b914f6d97e48301301731ed7846f8b6c880e
audishos <audisho.sada@gmail.com>
2017-10-16 13:39:34 -0400

    Implements authenticate calss method for user model and some unit tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,20 @@ class User < ApplicationRecord
 
   has_many :reviews
 
-  validates :email, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, length: { minimum: 8 }
+
+  before_save { self.email.downcase! unless self.email.blank? }
+
+  def self.authenticate_with_credentials(email, password)
+    @user = User.find_by(email: email.strip.downcase)
+    if @user && @user.authenticate(password)
+      @user
+    else
+      nil
+    end
+  end
 
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,74 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  context 'Validations:' do
+
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+    it { should have_secure_password }
+
+    it 'should fail when password is less than 8 characters' do
+      @user = User.new(
+        first_name: 'John',
+        last_name: 'Smith',
+        email: 'john@example.com',
+        password: '1234567',
+        password_confirmation: '1234567'
+      )
+      @user.save
+      expect(@user.id).to be_nil
+    end
+
+    it 'should pass when password is 8 characters' do
+      @user = User.new(
+        first_name: 'John',
+        last_name: 'Smith',
+        email: 'john@example.com',
+        password: '12345678',
+        password_confirmation: '12345678'
+      )
+      @user.save!
+      expect(@user.id).to be_present
+    end
+
+  end
+
+  context '.authenticate_with_credentials' do
+
+    before :all do
+      @user = User.new(
+        first_name: 'John',
+        last_name: 'Smith',
+        email: 'john@example.com',
+        password: 'correctpassword',
+        password_confirmation: 'correctpassword'
+      )
+      @user.save!
+    end
+
+    it 'should return the user john@example.com' do
+      user = User.authenticate_with_credentials('john@example.com', 'correctpassword')
+      expect(user.email).to eq('john@example.com')
+    end
+
+    it 'should return nil' do
+      user = User.authenticate_with_credentials('john@example.com', 'wrongpassword')
+      expect(user).to be_nil
+    end
+
+    it 'should return the user john@example.com' do
+      user = User.authenticate_with_credentials('   john@example.com   ', 'correctpassword')
+      expect(user.email).to eq('john@example.com')
+    end
+
+    it 'should return the user john@example.com' do
+      user = User.authenticate_with_credentials('jOhN@exaMplE.cOM', 'correctpassword')
+      expect(user.email).to eq('john@example.com')
+    end
+
+  end
+
+end
