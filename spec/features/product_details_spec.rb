a31d8a7c7aacdb4729b897adc1e9d0e9d0389a7f
audishos <audisho.sada@gmail.com>
2017-10-17 14:13:12 -0400

    Implements product deatils feature test

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.feature 'Visitor clicks on a product', type: :feature, js: true do
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario 'they click on the first product link' do
+
+    visit root_path
+    page.all('.product a')[0].click
+    save_screenshot
+    expect(page).to have_css('article.product-detail')
+
+  end
+
+
+end
